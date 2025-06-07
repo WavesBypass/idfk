9545a6e39a19e6f3c64cbbeaf9e50e9789f91af4
Hjr <john789811@gmail.com>
2025-06-07 16:49:09 +1000

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,15 @@
 const { Pool } = require('pg');
 
+// Accept self-signed certificates (for DigitalOcean)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for self-signed certs on DigitalOcean
-  }
+  ssl: true
 });
 
 pool.connect()
   .then(() => console.log('✅ Connected to PostgreSQL'))
-  .catch(err => {
-    console.error('❌ PostgreSQL connection error:', err.message);
-    process.exit(1); // Stop the app if DB fails
-  });
+  .catch(err => console.error('❌ PostgreSQL connection error:', err));
 
 module.exports = pool;
