2d66928cfca43f917235eac4dacb3722956bc007
Hjr <john789811@gmail.com>
2025-06-28 15:51:13 +1000

    Add files via upload

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 });
+
 module.exports = pool;
